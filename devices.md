3ff9e6e14521bba52895f78917c564fc8a7a5cfd
Lucas Dietrich <ld.adecy@gmail.com>
2022-03-17 15:05:47 +0000

    Discover Xiaomi primary handles

---
+++ devices.md
@@ -0,0 +1,9 @@
+# Devices
+
+| Name | MAC |
+| --- | --- |
+| `nrf9160dk_nrf52840` - SN `960050029` | `FA:76:2E:BD:9C:BF` |
+| `nrf52840dk_nrf52840` - SN `683339521` | `D3:5F:A1:2E:37:92` |
+| `nrf52840dk_nrf52840` - SN `683624946` | Sniffer |
+| Xiaomi LYWSD03MMC - `??` | `A4:C1:38:68:05:63` |
+| Xiaomi LYWSD03MMC - `Lucas` | `A4:C1:38:A7:30:C4` |
--- src/main.c
+++ src/main.c
@@ -13,9 +13,24 @@
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/hci.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/uuid.h>
+#include <bluetooth/gatt.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 bt_addr_le_t devices[10];
 uint32_t devices_count = 0U;
+
+static struct bt_gatt_discover_params discover_params;
+static struct bt_gatt_subscribe_params subscribe_params;
+
+// LYWSD03MMC
+const char target_str[] = "A4:C1:38:A7:30:C4"; // xiaomi
+// const char target_str[] = "FA:76:2E:BD:9C:BF"; // nrf9160dk_nrf52840
+
+static struct bt_conn *default_conn = NULL;
 
 bool exists(const bt_addr_le_t *new_addr)
 {
@@ -39,11 +54,109 @@ bool append(const bt_addr_le_t *new_addr)
 	return true;
 }
 
+void process_target(const bt_addr_le_t *addr)
+{
+	int ret = bt_conn_le_create(addr,
+				    BT_CONN_LE_CREATE_CONN,
+				    BT_LE_CONN_PARAM_DEFAULT,
+				    &default_conn);
+	LOG_DBG("bt_conn_le_create = %d", ret);
+}
+
+static uint8_t notify_func(struct bt_conn *conn,
+			   struct bt_gatt_subscribe_params *params,
+			   const void *data, uint16_t length)
+{
+	if (!data) {
+		printk("[UNSUBSCRIBED]\n");
+		params->value_handle = 0U;
+		return BT_GATT_ITER_STOP;
+	}
+
+	printk("[NOTIFICATION] data %p length %u\n", data, length);
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+static uint8_t discover_func(struct bt_conn *conn,
+			     const struct bt_gatt_attr *attr,
+			     struct bt_gatt_discover_params *params)
+{
+	if (!attr) {
+		printk("Discover complete\n");
+		(void)memset(params, 0, sizeof(*params));
+		return BT_GATT_ITER_STOP;
+	}
+
+	printk("[ATTRIBUTE] handle 0x%x\n", attr->handle);
+
+	return BT_GATT_ITER_CONTINUE;
+}
+
+static void connected(struct bt_conn *conn, uint8_t err)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	if (err) {
+		LOG_ERR("Failed to connect to %s (%u)", log_strdup(addr), err);
+
+		bt_conn_unref(default_conn);
+		default_conn = NULL;
+
+		return;
+	}
+
+	if (conn == default_conn) {
+		LOG_INF("Connected: %s\n", log_strdup(addr));
+
+		/* Discover procedures can be initiated with the use of bt_gatt_discover() API which takes the bt_gatt_discover_params struct which describes the type of discovery. The parameters also serves as a filter when setting the uuid field only attributes which matches will be discovered, in contrast setting it to NULL allows all attributes to be discovered. */
+		discover_params.uuid = NULL;
+		discover_params.func = discover_func;
+		discover_params.start_handle = BT_ATT_FIRST_ATTTRIBUTE_HANDLE;
+		discover_params.end_handle = BT_ATT_LAST_ATTTRIBUTE_HANDLE;
+		discover_params.type = BT_GATT_DISCOVER_PRIMARY;
+
+		err = bt_gatt_discover(default_conn, &discover_params);
+		if (err) {
+			printk("Discover failed(err %d)\n", err);
+			return;
+		}
+	}
+
+	// bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+}
+
+static void disconnected(struct bt_conn *conn, uint8_t reason)
+{
+	char addr[BT_ADDR_LE_STR_LEN];
+
+	if (conn != default_conn) {
+		return;
+	}
+
+	bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+
+	LOG_INF("Disconnected: %s (reason 0x%02x)\n", log_strdup(addr), reason);
+
+	bt_conn_unref(default_conn);
+	default_conn = NULL;
+}
+
+BT_CONN_CB_DEFINE(conn_callbacks) = {
+	.connected = connected,
+	.disconnected = disconnected,
+};
+
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad)
 {
 	if (exists(addr) == false) {
 		append(addr);
+
+		if (rssi > -55) {
+			printk("[ CLOSE ]");
+		}
 
 		char addr_str[BT_ADDR_LE_STR_LEN];
 		bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
@@ -85,11 +198,20 @@ void main(void)
 
 	printk("Scanning...\n");
 
-	k_sleep(K_SECONDS(30));
+	k_sleep(K_SECONDS(5));
 
 	err = bt_le_scan_stop();
 	printk("bt_le_scan_stop = %d\n", err);
 
 	// show number of devices found
 	printk("Found %d devices\n", devices_count);
+
+	// convert target_str to address and connect to it if it is in the devices list
+	bt_addr_le_t target_addr;
+	bt_addr_le_from_str(target_str, "public", &target_addr);
+	process_target(&target_addr);
+
+	for(;;) {
+		k_sleep(K_SECONDS(1));
+	}
 }
