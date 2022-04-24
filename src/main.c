8d7c68d8f729e38f51e2b955099621d44be1198d
Lucas Dietrich <ld.adecy@gmail.com>
2022-04-24 09:07:46 +0000

    Clear project for IPC optimization

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 #include <zephyr.h>
 
+#include <string.h>
+
+#include "ipc_uart/ipc.h"
+
+#define MESSAGE "Hello World!"
+
 int main(void)
 {
+	ipc_data_t data;
+
+	memcpy(data.buf, MESSAGE, sizeof(MESSAGE));
+	data.size = sizeof(MESSAGE);
+
 	for (;;) {
-		k_sleep(K_SECONDS(1));
+		ipc_send_data(&data);
+
+		k_sleep(K_SECONDS(5));
 	}
 }
