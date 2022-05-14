b57c87a06ef899fb5a77e7a430d750dae2aea619
Lucas Dietrich <ld.adecy@gmail.com>
2022-05-14 06:17:36 +0000

    1 PWM peripheral per blinking led

--- src/main.c
+++ src/main.c
@@ -1,51 +1,92 @@
 /*
  * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2020 Nordic Semiconductor ASA
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/**
+ * @file Sample app to demonstrate PWM.
+ */
+
 #include <zephyr.h>
+#include <sys/printk.h>
 #include <device.h>
-#include <devicetree.h>
-#include <drivers/gpio.h>
+#include <drivers/pwm.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define PWM_LED0_NODE	DT_ALIAS(pwm_led3)
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#if DT_NODE_HAS_STATUS(PWM_LED0_NODE, okay)
+#define PWM_CTLR	DT_PWMS_CTLR(PWM_LED0_NODE)
+#define PWM_CHANNEL	DT_PWMS_CHANNEL(PWM_LED0_NODE)
+#define PWM_FLAGS	DT_PWMS_FLAGS(PWM_LED0_NODE)
 #else
-/* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
+#error "Unsupported board: pwm-led0 devicetree alias is not defined"
+#define PWM_CTLR	DT_INVALID_NODE
+#define PWM_CHANNEL	0
+#define PWM_FLAGS	0
 #endif
+
+#define MIN_PERIOD_USEC	(USEC_PER_SEC / 64U)
+#define MAX_PERIOD_USEC	USEC_PER_SEC
 
 void main(void)
 {
-	const struct device *dev;
-	bool led_is_on = true;
+	const struct device *pwm;
+	uint32_t max_period;
+	uint32_t period;
+	uint8_t dir = 0U;
 	int ret;
 
-	dev = device_get_binding(LED0);
-	if (dev == NULL) {
+	printk("PWM-based blinky\n");
+
+	pwm = DEVICE_DT_GET(PWM_CTLR);
+	if (!device_is_ready(pwm)) {
+		printk("Error: PWM device %s is not ready\n", pwm->name);
 		return;
 	}
 
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-	if (ret < 0) {
-		return;
+	/*
+	 * In case the default MAX_PERIOD_USEC value cannot be set for
+	 * some PWM hardware, decrease its value until it can.
+	 *
+	 * Keep its value at least MIN_PERIOD_USEC * 4 to make sure
+	 * the sample changes frequency at least once.
+	 */
+	printk("Calibrating for channel %d...\n", PWM_CHANNEL);
+	max_period = MAX_PERIOD_USEC;
+	while (pwm_pin_set_usec(pwm, PWM_CHANNEL,
+				max_period, max_period / 2U, PWM_FLAGS)) {
+		max_period /= 2U;
+		if (max_period < (4U * MIN_PERIOD_USEC)) {
+			printk("Error: PWM device "
+			       "does not support a period at least %u\n",
+			       4U * MIN_PERIOD_USEC);
+			return;
+		}
 	}
 
+	printk("Done calibrating; maximum/minimum periods %u/%u usec\n",
+	       max_period, MIN_PERIOD_USEC);
+
+	period = max_period;
 	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
-		k_msleep(SLEEP_TIME_MS);
+		ret = pwm_pin_set_usec(pwm, PWM_CHANNEL,
+				       period, period / 2U, PWM_FLAGS);
+		if (ret) {
+			printk("Error %d: failed to set pulse width\n", ret);
+			return;
+		}
+
+		period = dir ? (period * 2U) : (period / 2U);
+		if (period > max_period) {
+			period = max_period / 2U;
+			dir = 0U;
+		} else if (period < MIN_PERIOD_USEC) {
+			period = MIN_PERIOD_USEC * 2U;
+			dir = 1U;
+		}
+
+		k_sleep(K_SECONDS(4U));
 	}
 }
